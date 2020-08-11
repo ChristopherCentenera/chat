e2a00c61f2cd0d60252b87bdbfb221b8612904df
Christopher Centenera <Christopher_Centenera@hotmail.com>
2020-08-11 11:59:52 -0700

    changed port

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const express = require('express');
 
 app.get('/',(req,res) => {
     res.sendFile(__dirname + "/index.html");
 });
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
     socket.on('disconnect', () => {
         console.log('disconnected');
     })
 })
 
-http.listen(3000,() => {
-    console.log('Listening on port 3000');
+http.listen(3001,() => {
+    console.log('Listening on port 3001');
 });
